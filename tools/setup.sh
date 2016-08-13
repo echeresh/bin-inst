6dec69be172e4daa54596b9e775d53c1deca270b
Eugene Chereshnev <eugenechereshnev@gmail.com>
2016-08-13 13:05:35 -0400

    Fix path to pin

--- tools/setup.sh
+++ tools/setup.sh
@@ -3,6 +3,6 @@
 base="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 if [ "$PIN_ROOT" == "" ]; then
-export PIN_ROOT=/root/ms/pin
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${base}/../../../pin/intel64/lib-ext:${base}/../../../pin/intel64/runtime
+export PIN_ROOT=${base}/../../pin
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${PIN_ROOT}/intel64/lib-ext:${PIN_ROOT}/intel64/runtime
 fi
